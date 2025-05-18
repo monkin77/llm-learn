9bf9011ad711c9b660e0e84aa87b299de547a49d
Monkin <monkinsmurf@gmail.com>
2025-05-18 17:01:02 +0100

    Add: Read File Tool to the agent

---
+++ code-editing-agent/README.md
@@ -0,0 +1,4 @@
+# Code Editing Agent
+This Repository contains an example of an agent relying on Claude.
+
+It follows the tutorial from [this website](https://ampcode.com/how-to-build-an-agent?utm_source=tldrnewsletter).
--- code-editing-agent/main.go
+++ code-editing-agent/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/anthropics/anthropic-sdk-go"
+
+	"github.com/invopop/jsonschema"
 )
 
 func main() {
@@ -23,8 +26,11 @@ func main() {
 		return scanner.Text(), true
 	}
 
+	// Define the tools that the agent can use
+	tools := []ToolDefinition{ReadFileDefinition}
+
 	// Create a new agent with the Anthropics client and the user message function
-	agent := NewAgent(&client, getUserMessage)
+	agent := NewAgent(&client, getUserMessage, tools)
 
 	// Run the agent in a context. The context is used to manage the lifecycle of the request.
 	err := agent.Run(context.TODO())
@@ -33,7 +39,7 @@ func main() {
 	}
 }
 
-func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool), tools []ToolDefinition) *Agent {
 	/*
 		Initialize a new agent with the Anthropics client and the user message function.
 		The client is used to make requests to the Anthropics API, and the getUserMessage function
@@ -47,37 +53,33 @@ func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *A
 	return &Agent{
 		client:         client,
 		getUserMessage: getUserMessage,
+		tools:		  	tools,
 	}
 }
 
 type Agent struct {
 	client         *anthropic.Client
 	getUserMessage func() (string, bool)
+	tools          []ToolDefinition
 }
 
 func (a *Agent) Run(ctx context.Context) error {
-	/*
-		Params:
-		- ctx: The context to use for the request. The context is used to manage the lifecycle of the request.
-		Returns:
-		- error: An error if the request fails, nil otherwise.
-	*/
 	conversation := []anthropic.MessageParam{}
 
 	fmt.Println("Chat with Claude (use 'ctrl-c' to quit)")
 
-	// Loop to get user input and send it to the Anthropics API
-	// The loop continues until the user quits or an error occurs.
+	readUserInput := true	// Flag to indicate whether to read user input on the next iteration or not
 	for {
-		fmt.Print("\u001b[94mYou\u001b[0m: ")
-		userInput, ok := a.getUserMessage()
-		if !ok {
-			break
-		}
+		if readUserInput {
+			fmt.Print("\u001b[94mYou\u001b[0m: ")
+			userInput, ok := a.getUserMessage()
+			if !ok {
+				break
+			}
 
-		// Build a userMessage with the user input and add it to the conversation
-		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
-		conversation = append(conversation, userMessage)
+			userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+			conversation = append(conversation, userMessage)
+		}
 
 		message, err := a.runInference(ctx, conversation)
 		if err != nil {
@@ -85,22 +87,126 @@ func (a *Agent) Run(ctx context.Context) error {
 		}
 		conversation = append(conversation, message.ToParam())
 
+		toolResults := []anthropic.ContentBlockParamUnion{}
 		for _, content := range message.Content {
 			switch content.Type {
 			case "text":
 				fmt.Printf("\u001b[93mClaude\u001b[0m: %s\n", content.Text)
+			case "tool_use":
+				result := a.executeTool(content.ID, content.Name, content.Input)
+				toolResults = append(toolResults, result)
 			}
 		}
+
+		if len(toolResults) == 0 {
+			// No tool results, so we can continue to the next User Prompt
+			readUserInput = true
+			continue
+		}
+
+		// We have tool results -> we need to send them back to Claude instead of waiting for user input
+		readUserInput = false
+		conversation = append(conversation, anthropic.NewUserMessage(toolResults...))
 	}
 
 	return nil
 }
 
+func (a *Agent) executeTool(id, name string, input json.RawMessage) anthropic.ContentBlockParamUnion {
+	var toolDef ToolDefinition
+	var found bool
+	for _, tool := range a.tools {
+		if tool.Name == name {
+			toolDef = tool
+			found = true
+			break
+		}
+	}
+	if !found {
+		return anthropic.NewToolResultBlock(id, "tool not found", true)
+	}
+
+	fmt.Printf("\u001b[92mtool\u001b[0m: %s(%s)\n", name, input)
+	response, err := toolDef.Function(input)
+	if err != nil {
+		return anthropic.NewToolResultBlock(id, err.Error(), true)
+	}
+	return anthropic.NewToolResultBlock(id, response, false)
+}
+
 func (a *Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (*anthropic.Message, error) {
+	anthropicTools := []anthropic.ToolUnionParam{}
+
+	// Add the Agent's tools to the anthropicTools slice
+	for _, tool := range a.tools {
+		anthropicTools = append(anthropicTools, anthropic.ToolUnionParam{
+			OfTool: &anthropic.ToolParam{
+				Name: tool.Name,
+				Description: anthropic.String(tool.Description),
+				InputSchema: tool.InputSchema,
+			},
+		})
+	}
+
 	message, err := a.client.Messages.New(ctx, anthropic.MessageNewParams{
 		Model:     anthropic.ModelClaude3_7SonnetLatest, // Define the model to use for inference
 		MaxTokens: int64(1024),                          // Define the maximum number of tokens to generate
 		Messages:  conversation,                         // Send the conversation history to Claude's model
+		Tools: anthropicTools, 							 // Send the tools to Claude's model
 	})
+
 	return message, err
+}
+
+// Define the Tools that the agent can use
+type ToolDefinition struct {
+	Name        string                         `json:"name"`
+	Description string                         `json:"description"`
+	InputSchema anthropic.ToolInputSchemaParam `json:"input_schema"`
+	Function    func(input json.RawMessage) (string, error)
+}
+
+// Define the ReadFile Tool
+var ReadFileDefinition = ToolDefinition{
+	Name: "read_file",
+	Description: "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
+	InputSchema: ReadFileInputSchema,
+	Function: ReadFile,
+}
+
+type ReadFileInput struct {
+	Path string `json:"path" jsonschema_description:"The relative path of a file in the working directory."`
+}
+
+var ReadFileInputSchema = GenerateSchema[ReadFileInput]()
+
+func GenerateSchema[T any]() anthropic.ToolInputSchemaParam {
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+
+	schema := reflector.Reflect(v)
+
+	return anthropic.ToolInputSchemaParam{
+		Properties: schema.Properties,
+	}
+}
+
+func ReadFile(input json.RawMessage) (string, error) {
+	/*
+		This functions reads the contents of a file at the given path and returns it as a string.
+	*/
+	readFileInput := ReadFileInput{}
+	err := json.Unmarshal(input, &readFileInput)
+	if err != nil {
+		panic(err)
+	}
+
+	content, err := os.ReadFile(readFileInput.Path)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
 }
